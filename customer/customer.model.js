cf21eaf53abfa99ab1a037c6d95805ed59c95a19
olushola <olushola.dauda@algorismng.com>
2021-08-20 18:22:05 +0100

    setting up customer service

---
+++ customer/customer.model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+// Customer Model
+
+mongoose.model("Customer", {
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+  },
+});
